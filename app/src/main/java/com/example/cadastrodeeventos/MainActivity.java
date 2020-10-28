befce4dd747843a713b53312547c870db34a2918
douglasantunesd <70909360+douglasantunesd@users.noreply.github.com>
2020-10-28 13:20:54 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/cadastrodeeventos/MainActivity.java
+++ app/src/main/java/com/example/cadastrodeeventos/MainActivity.java
@@ -13,9 +13,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.cadastrodeeventos.database.EventoDAO;
 import com.example.cadastrodeeventos.model.Evento;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,35 +27,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Eventos");
-
         listViewEventos = findViewById(R.id.listView_eventos);
-        ArrayList<Evento> eventos = new ArrayList<>();
-        adapterEventos = new ArrayAdapter<Evento>(MainActivity.this, android.R.layout.simple_list_item_1, eventos);
-        listViewEventos.setAdapter(adapterEventos);
 
         definirOnClickListenerListView();
         definirOnLongClickListener();
-
-
     }
 
-    private void definirOnLongClickListener(){
+    @Override
+    protected void onResume() {
+        super.onResume();
+        EventoDAO eventoDAO = new EventoDAO(getBaseContext());
+        adapterEventos = new ArrayAdapter<Evento>(MainActivity.this, android.R.layout.simple_list_item_1, eventoDAO.listar());
+        listViewEventos.setAdapter(adapterEventos);
+    }
+
+    private void definirOnLongClickListener() {
         listViewEventos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                final Evento evento = adapterEventos.getItem(position);
+                final Evento eventoClicado = adapterEventos.getItem(position);
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setIcon(android.R.drawable.ic_delete);
-                builder.setTitle("Removendo Evento!");
-                builder.setMessage("Realmente deseja remover o evento?");
-                builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        adapterEventos.remove(evento);
-                        adapterEventos.notifyDataSetChanged();
-                        Toast.makeText(MainActivity.this, "Evento excluído com sucesso", Toast.LENGTH_LONG).show();
-                    } } );
-                builder.setNegativeButton("Nào", null).show();
+                        builder.setIcon(android.R.drawable.ic_delete);
+                        builder.setTitle("Removendo Evento!");
+                        builder.setMessage("Deseja realmente remover este Evento?");
+                        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                EventoDAO eventoDAO = new EventoDAO(getBaseContext());
+                                eventoDAO.deletar(eventoClicado);
+                                Toast.makeText(MainActivity.this, "Evento removido com sucesso!", Toast.LENGTH_LONG).show();
+                                onResume();
+                            }
+                        })
+                        .setNegativeButton("Não",null).show();
                 return true;
             }
         });
@@ -69,34 +73,34 @@ public class MainActivity extends AppCompatActivity {
                 Evento eventoClicado = adapterEventos.getItem(position);
                 Intent intent = new Intent(MainActivity.this, CadastroEventoActivity.class);
                 intent.putExtra("eventoEdicao", eventoClicado);
-                startActivityForResult(intent, 2);
+                startActivity(intent);
             }
         });
     }
 
     public void onClickCadastrarEvento(View view){
         Intent intent = new Intent(MainActivity.this, CadastroEventoActivity.class);
-        startActivityForResult(intent, 1);
+        startActivity(intent);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1 && resultCode == 10){
-            Evento evento = (Evento) data.getExtras().getSerializable("novoEvento");
+           Evento evento = (Evento) data.getExtras().getSerializable("novoEvento");
             evento.setId(++id);
             adapterEventos.add(evento);
         } else if(requestCode == 2 && resultCode == 11){
             Evento eventoEditado = (Evento) data.getExtras().getSerializable("eventoEditado");
             for (int i = 0; i < adapterEventos.getCount(); ++i){
                 Evento evento = adapterEventos.getItem(i);
-                if(evento.getId() == eventoEditado.getId()){
+               if(evento.getId() == eventoEditado.getId()){
                     adapterEventos.remove(evento);
                     adapterEventos.insert(eventoEditado, i);
                     break;
                 }
 
-            }
-        }
-    }
+           }
+       }
+   }
 }
